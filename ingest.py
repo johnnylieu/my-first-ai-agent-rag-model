98af1f6fbac0fea2d16efe51e5c7ae8a795595e6
Johnny <JohnnyLieu@yahoo.com>
2026-04-04 19:07:23 -0700

    get_loader returns correct loader for file type

--- ingest.py
+++ ingest.py
@@ -5,8 +5,35 @@ from langchain_community.document_loaders import (
     Docx2txtLoader,
     CSVLoader,
     UnstructuredHTMLLoader,
-    UnstructuredPowerPointLoader
+    UnstructuredPowerPointLoader,
+    UnstructuredMarkdownLoader
 )
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_ollama import OllamaEmbeddings
 from langchain_community.vectorstores import Chroma
+
+DOCUMENTS_PATH = "documents"
+CRHOMA_PATH = "chroma_db"
+
+# returns correct loader based off file type
+# else returns None if file type is not supported
+def get_loader(file_path):
+    extension = os.path.splitext(file_path)[1].lower()
+
+    loaders = {
+        ".pdf": PyPDFLoader,
+        ".txt": TextLoader,
+        ".docx": Docx2txtLoader,
+        ".csv": CSVLoader,
+        ".html": UnstructuredHTMLLoader,
+        ".md": UnstructuredMarkdownLoader,
+        ".pptx": UnstructuredPowerPointLoader
+    }
+
+    loader_class = loaders.get(extension)
+
+    if loader_class is None:
+        print(f"Unsupported file type: {extension}")
+        return None
+    
+    return loader_class(file_path)
