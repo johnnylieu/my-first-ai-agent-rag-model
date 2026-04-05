3add50ef4506f600824afe5f8cc1ef175758686b
Johnny <JohnnyLieu@yahoo.com>
2026-04-04 20:22:22 -0700

    imports for agent.py

---
+++ agent.py
@@ -0,0 +1,6 @@
+import os
+from langchain_ollama import OllamaEmbeddings, ChatOllama
+from langchain_community.vectorstores import Chroma
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
